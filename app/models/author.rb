2c5d6f5435af40b690e4ee2b695ecb16b3a318d4
Robinson <robinadoro@gmail.com>
2022-08-11 10:56:50 +0300

    commit tree e00fa02bff71d53c896ef2614b2d1958c14b94dd

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, length: {minimum: 2}
+    validates :phone_number, presence: true, length: {is: 10}
+    
+    
+    
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    post = Post.new
+    post.valid?
+
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 250}
+    validates :summary, presence: true, length: {maximum: 250}
+    validates :category, presence: true, inclusion: {in: %w(Fiction Non-Fiction)}
+    
+    
+
+    
+    
+    
+    
 end
